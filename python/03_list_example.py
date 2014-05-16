7c69f5280b75c52c30b30ada6b98a160f82a4995
Harry Chou <harrychou@gmail.com>
2014-05-15 21:46:32 -0400

    Create 03_list_example.py

---
+++ python/03_list_example.py
@@ -0,0 +1,14 @@
+import sys # Load the sys module
+
+if len(sys.argv) != 2: # Check number of command line arguments :
+	print("Please supply a filename")
+	raise SystemExit
+
+f = open(sys.argv[1]) # Filename on the command line
+svalues = f.readlines() # Read all lines into a list
+f.close()
+# Convert all of the input values from strings to floats
+fvalues = [float(s) for s in svalues]
+# Print min and max values
+print ("The minimum value is ", min(fvalues))
+print ("The maximum value is ", max(fvalues))
